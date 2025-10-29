90679194c8754852571f1e84bdfea6d3237c25e6
pondol <y.h.ryu@tipszone.co.kr>
2025-10-29 17:24:37 +0900

    t
    t

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -3,53 +3,49 @@
 namespace Pondol\IndexNow\Console\Commands;
 
 use Illuminate\Console\Command;
-// use Illuminate\Filesystem\Filesystem;
-// use Illuminate\Support\Str;
-// use Symfony\Component\Process\PhpExecutableFinder;
-// use Symfony\Component\Process\Process;
 
 class InstallCommand extends Command
 {
-  // use InstallsBladeStack;
+    // use InstallsBladeStack;
 
-  /**
-   * The name and signature of the console command.
-   *
-   * @var string
-   */
-  protected $signature = 'pondol:install-index-now {type=full}'; // full | only
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pondol:install-index-now {type=full}'; // full | only
 
-  /**
-   * The console command description.
-   *
-   * @var string
-   */
-  protected $description = "Install Pondol's Index Now";
-
-
-  public function __construct()
-  {
-    parent::__construct();
-  }
-
-  public function handle()
-  {
-    $type = $this->argument('type');
-    return $this->installLaravelMailer($type);
-  }
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Install Pondol's Index Now";
 
 
-  private function installLaravelMailer($type)
-  {
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-    \Artisan::call('vendor:publish',  [
-      '--force'=> true,
-      '--provider' => 'Pondol\IndexNow\IndexNowServiceProvider'
-    ]);
-    
-    $this->info("The pondol's laravel index-now installed successfully.");
-    
-  }
+    public function handle()
+    {
+        $type = $this->argument('type');
+        return $this->installLaravelIndexNow($type);
+    }
+
+
+    private function installLaravelIndexNow($type)
+    {
+
+        \Artisan::call('vendor:publish', [
+          '--force' => true,
+          '--provider' => 'Pondol\IndexNow\IndexNowServiceProvider'
+        ]);
+
+        $this->info("The pondol's laravel index-now installed successfully.");
+
+    }
 
 
 }
